340a1f3c2943e6f164014a97fec8a5d89e465396
Thomas Billot <thomas.billot@awstudio.fr>
2023-11-24 16:33:27 +0100

    [WIP] Plan entity

--- src/DependencyInjection/MotherbrainSyliusProductSubscriptionExtension.php
+++ src/DependencyInjection/MotherbrainSyliusProductSubscriptionExtension.php
@@ -9,7 +9,7 @@ use Sylius\Bundle\ResourceBundle\DependencyInjection\Extension\AbstractResourceE
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 final class MotherbrainSyliusProductSubscriptionExtension extends AbstractResourceExtension implements PrependExtensionInterface
 {
@@ -18,9 +18,9 @@ final class MotherbrainSyliusProductSubscriptionExtension extends AbstractResour
     /** @psalm-suppress UnusedVariable */
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
 
-        $loader->load('services.xml');
+        $loader->load('services.yaml');
     }
 
     public function prepend(ContainerBuilder $container): void
--- src/Form/Type/PlanType.php
+++ src/Form/Type/PlanType.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Motherbrain\SyliusProductSubscriptionPlugin\Form\Type;
 
 use Motherbrain\SyliusProductSubscriptionPlugin\Entity\Plan;
+use Sylius\Bundle\ProductBundle\Form\Type\ProductChoiceType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,6 +15,11 @@ final class PlanType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $builder
+            ->add('name', TextType::class)
+            ->add('products', ProductChoiceType::class, [
+                'multiple' => true
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
